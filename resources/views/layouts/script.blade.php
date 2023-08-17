49cea9e5c2fe3786f20d0ba9456e320e9745072d
duong8888 <anhduongpro16032003@gmail.com>
2023-08-17 16:10:42 +0700

    cát layout hiện thị

---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,12 @@
+<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script src="{{asset('assets/js/jquery-3.6.0.min.js')}}"></script>
+
+<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+
+<script src="{{asset('assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+
+<script src="{{asset('assets/plugins/swiper/swiper.min.js')}}"></script>
+
+<script src="{{asset('assets/plugins/fancybox/jquery.fancybox.min.js')}}"></script>
+
+<script src="{{asset('assets/js/script.js')}}"></script>
---
+++ resources/views/layouts/style.blade.php
@@ -0,0 +1,12 @@
+<link rel="icon" href="{{asset('assets/img/favicon.png')}}">
+
+<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+
+<link rel="stylesheet" href="{{asset('assets/plugins/fontawesome/css/fontawesome.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/plugins/fontawesome/css/all.min.css')}}">
+
+<link rel="stylesheet" href="{{asset('assets/plugins/swiper/swiper.min.css')}}">
+
+<link rel="stylesheet" href="{{asset('assets/plugins/fancybox/jquery.fancybox.min.css')}}">
+
+<link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
